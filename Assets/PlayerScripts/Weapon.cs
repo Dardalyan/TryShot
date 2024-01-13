e02b87fd79dcbe4650aedca912332edda7f8d565
ardasenyuz <ardasenyuz@gmail.com>
2024-01-13 12:49:37 +0300

    Differentiated weapons by range, damage and time between shoots

--- Assets/PlayerScripts/Weapon.cs
+++ Assets/PlayerScripts/Weapon.cs
@@ -7,36 +7,48 @@ public class Weapon : MonoBehaviour
     [SerializeField] private Transform cam;
     [SerializeField] private float range = 100f;
     [SerializeField] private Ammo ammoSlot;
-    //damage variable
+    [SerializeField] private float timeBetweenShots = 0.5f;
+    [SerializeField] private float damage = 25;
+
+    private bool canShoot = true;
     
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if(Input.GetMouseButtonDown(0) && canShoot == true)
         {
-            Shoot();
+            StartCoroutine(Shoot());
         }
     }
 
-    private void Shoot()
+    IEnumerator Shoot()
     {
+        canShoot = false;
+        
         if (ammoSlot.GetCurrentAmmo() > 0)
         {
-            RaycastHit hit;
-            if(Physics.Raycast(cam.position, cam.forward, out hit, range))
-            {
-                Debug.Log("Hit: " + hit.transform.name);
-                //add shooting effect
-                //set a target (getcomponent enemy health)
-                //if (target == null){return;}
-                //call enemy healt decrease method
-            }
-            else
-            {
-                return;
-            }
-        
+            ProcessRaycast();
             ammoSlot.ReduceCurrentAmmo();
         }
-      
+
+        yield return new WaitForSeconds(timeBetweenShots);
+        
+        canShoot = true;
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(cam.position, cam.forward, out hit, range))
+        {
+            Debug.Log("Hit: " + hit.transform.name);
+            //add shooting effect
+            //set a target (getcomponent enemy health)
+            //if (target == null){return;}
+            //call enemy healt decrease method
+        }
+        else
+        {
+            return;
+        }
     }
 }
