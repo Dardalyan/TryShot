659589342f4993cc0a015abb97ce0708084916bc
ardasenyuz <ardasenyuz@gmail.com>
2024-01-14 13:34:08 +0300

    Added health in the ui

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float hitPoints = 100f;
 
+    [SerializeField] private TextMeshProUGUI healthText;
+
+    private void Start()
+    {
+        healthText.text = Mathf.Floor(hitPoints).ToString();
+    }
+
     public void TakeDamage(float damage)
     {
         hitPoints -= damage;
+        healthText.text = Mathf.Floor(hitPoints).ToString();
         if (hitPoints <= 0)
         {
             GetComponent<DeathHandler>().HandleDeath();
